f402c5d7acbb6100c1e505534826d68c64c79eb9
Rakka Rage <rakkarage@gmail.com>
2023-05-10 20:40:50 -0400

    first

---
+++ Test.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var _sprite1 := $Sprite1
+@onready var _sprite2 := $Sprite2
+
+const _speed := 333
+const _size := 1024
+const _reset := -1024
+
+func _process(delta) -> void:
+	_sprite1.position.x += _speed * delta
+	_sprite2.position.x += _speed * delta
+	if _sprite1.position.x > _size:
+		_sprite1.position.x = _reset
+	if _sprite2.position.x > _size:
+		_sprite2.position.x = _reset
+	pass
